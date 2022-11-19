7b61eca39e4473f8791f4fbb3f0776c35934bde9
Aman Khan <79637672+amankhangit@users.noreply.github.com>
2022-11-19 14:49:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,6 @@
             update: function(event, ui) {
                 getIdsOfImages();
             }//end update         
-            });
+            })
         });
-          
-        function getIdsOfImages() {
-            var values = [];
-            $('.listitemClass').each(function (index) {
-                values.push($(this).attr("id")
-                        .replace("imageNo", ""));
-            });
-              
-            $('#outputvalues').val(values);
-        }
     </script>
