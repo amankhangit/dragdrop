2331bfc6dc8dd12ec56f66b97b55ce2f4e24ab7c
Aman Khan <amankhann2023@gmail.com>
2022-11-19 14:11:35 +0530

    files added

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+        $(function() {
+            $( "#imageListId" ).sortable({
+            update: function(event, ui) {
+                getIdsOfImages();
+            }//end update         
+            });
+        });
+          
+        function getIdsOfImages() {
+            var values = [];
+            $('.listitemClass').each(function (index) {
+                values.push($(this).attr("id")
+                        .replace("imageNo", ""));
+            });
+              
+            $('#outputvalues').val(values);
+        }
+    </script>
